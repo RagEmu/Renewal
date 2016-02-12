4fec5df8ef33faa489c68804c1017aff9b42c277
Haru <haru@dotalux.com>
2016-02-12 16:17:30 +0100

    Ported socket.conf (was packet.conf) to libconfig
    Ported to modern Hercules and cleaned up from Panikon's commits: b10f2a88073882bea812c6278fdec7848102ab87, bc5007299a92898ea4630f2a977d9a1b42b6c145
    
    Signed-off-by: Haru <haru@dotalux.com>

--- src/char/char.c
+++ src/char/char.c
@@ -6300,6 +6300,7 @@ int do_init(int argc, char **argv) {
 
 		CHECK_OLD_LOCAL_CONF("conf/import/char_conf.txt", "conf/import/char-server.conf");
 		CHECK_OLD_LOCAL_CONF("conf/import/inter_conf.txt", "conf/import/inter-server.conf");
+		CHECK_OLD_LOCAL_CONF("conf/import/packet_conf.txt", "conf/import/socket.conf");
 
 #undef CHECK_OLD_LOCAL_CONF
 	}
