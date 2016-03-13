c62d0093adfb29bdd6b2c3fb2a9a640046fca922
Haru <haru@dotalux.com>
2016-03-13 15:48:14 +0100

    Removed unnecessary typedefs from thread and spinlock
    - SPIN_LOCK -> struct spin_lock
    - rAthread -> struct thread_handle
    - rAthreadProc -> threadFunc
    - RATHREAD_PRIO -> enum thread_priority
    - RAT_PRIO_LOW -> THREADPRIO_LOW
    - RAT_PRIO_NORMAL -> THREADPRIO_NORMAL
    - RAT_PRIO_HIGH -> THREADPRIO_HIGH
    - RA_THREADS_MAX -> THREADS_MAX
    
    Signed-off-by: Haru <haru@dotalux.com>
    
    fixupthread

--- src/test/test_spinlock.c
+++ src/test/test_spinlock.c
@@ -39,7 +39,7 @@
 #define PERINC 100000
 #define LOOPS 47
 
-static SPIN_LOCK lock;
+static struct spin_lock lock;
 static unsigned int val = 0;
 static volatile int32 done_threads = 0;
 
@@ -61,8 +61,9 @@ static  void *worker(void *p){
 	return NULL;
 }//end: worker()
 
-int do_init(int argc, char **argv){
-	rAthread *t[THRC];
+int do_init(int argc, char **argv)
+{
+	struct thread_handle *t[THRC];
 	int j, i;
 	int ok;
 
@@ -79,7 +80,7 @@ int do_init(int argc, char **argv){
 		InitializeSpinLock(&lock);
 
 		for(i =0; i < THRC; i++){
-			t[i] = thread->createEx(worker, NULL, 1024*512, RAT_PRIO_NORMAL);
+			t[i] = thread->createEx(worker, NULL, 1024*512, THREADPRIO_NORMAL);
 		}
 
 		while(1){
