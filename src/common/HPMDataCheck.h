2eefc43d660bcb8f5d76f40b78ef2be556665359
Dastgir <dastgirpojee@rocketmail.com>
2016-04-03 05:27:08 -0400

    Header Fixes

--- src/common/HPMDataCheck.h
+++ src/common/HPMDataCheck.h
@@ -1,10 +1,9 @@
 /**
- * This file is part of Hercules.
- * http://herc.ws - http://github.com/HerculesWS/Hercules
+ * This file is part of RagEmu.
+ * http://ragemu.org - https://github.com/RagEmu/Renewal
  *
- * Copyright (C) 2014-2016  Hercules Dev Team
- *
- * Hercules is free software: you can redistribute it and/or modify
+ * Copyright (C) 2016  RagEmu Dev Team
+ * RagEmu is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
