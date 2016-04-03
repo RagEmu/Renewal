0a92946fae30825fe57ab8e438f366b65fcc4164
Jedzkie <jedzkie13@rocketmail.com>
2016-04-03 23:09:46 +0800

    Implemented official behavior of AC_MAKINGARROW  - It will not show a blank list if there are no available items to be craft in your inventory  - It will not show skill animation when the skill is failed.

--- src/map/skill.c
+++ src/map/skill.c
@@ -6170,9 +6170,8 @@ int skill_castend_nodamage_id(struct block_list *src, struct block_list *bl, uin
 			break;
 
 		case AC_MAKINGARROW:
-			if(sd) {
+			if (sd) {
 				clif->arrow_create_list(sd);
-				clif->skill_nodamage(src,bl,skill_id,skill_lv,1);
 			}
 			break;
 
