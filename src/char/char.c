7d693004eb27511a5bb44f61b0892d43704dae74
Jedzkie <jedzkie13@rocketmail.com>
2016-04-07 12:55:36 +0800

    Follow up https://github.com/RagEmu/Renewal/commit/19d485f827cf89e88f88a4c304ed19f138bca374

--- src/char/char.c
+++ src/char/char.c
@@ -5524,7 +5524,7 @@ int char_config_read(const char* cfgName)
 		} else if (strcmpi(w1, "save_log") == 0) {
 			save_log = config_switch(w2);
 		}
-		else if (strcmpi(w1, "start_point_re") == 0) {
+		else if (strcmpi(w1, "start_point") == 0) {
 			char map[MAP_NAME_LENGTH_EXT];
 			int x, y;
 			if (sscanf(w2, "%15[^,],%d,%d", map, &x, &y) < 3)
