49ca20447a642008dee725cf90558150924d2a4a
Dastgir <dastgirp@gmail.com>
2016-04-25 18:15:00 +0530

    Added support for 2015-11-04aRagexe

--- src/map/packets.h
+++ src/map/packets.h
@@ -3022,6 +3022,39 @@ packet(0x96e,-1,clif->ackmergeitems);
 	packet(0x0a39,36);
 #endif
 
+// 2015-11-04aRagexe
+#if PACKETVER >= 20151104
+	packet(0x0369,7,clif->pActionRequest,2,6);
+	packet(0x083C,10,clif->pUseSkillToId,2,4,6);
+	packet(0x0363,5,clif->pWalkToXY,2);
+	packet(0x0886,6,clif->pTickSend,2);
+	packet(0x0928,5,clif->pChangeDir,2,4);
+	packet(0x0964,6,clif->pTakeItem,2);
+	packet(0x0437,6,clif->pDropItem,2,4);
+	packet(0x088B,8,clif->pMoveToKafra,2,4);
+	packet(0x0364,8,clif->pMoveFromKafra,2,4);
+	packet(0x0438,10,clif->pUseSkillToPos,2,4,6,8);
+	packet(0x0366,90,clif->pUseSkillToPosMoreInfo,2,4,6,8,10);
+	packet(0x0887,6,clif->pGetCharNameRequest,2);
+	packet(0x0368,6,clif->pSolveCharName,2);
+	packet(0x0838,12,clif->pSearchStoreInfoListItemClick,2,6,10);
+	packet(0x0835,2,clif->pSearchStoreInfoNextPage,0);
+	packet(0x0819,-1,clif->pSearchStoreInfo,2,4,5,9,13,14,15);
+	packet(0x0815,-1,clif->pReqTradeBuyingStore,2,4,8,12);
+	packet(0x0436,6,clif->pReqClickBuyingStore,2);
+	packet(0x0817,2,clif->pReqCloseBuyingStore,0);
+	packet(0x023B,-1,clif->pReqOpenBuyingStore,2,4,8,9,89);
+	packet(0x0811,18,clif->pPartyBookingRegisterReq,2,4);
+	packet(0x0939,8); // CZ_JOIN_BATTLE_FIELD
+	packet(0x093A,-1,clif->pItemListWindowSelected,2,4,8);
+	packet(0x0360,19,clif->pWantToConnection,2,6,10,14,18);
+	packet(0x08A5,26,clif->pPartyInvite2,2);
+	packet(0x08A3,4); // CZ_GANGSI_RANK
+	packet(0x07EC,26,clif->pFriendsListAdd,2);
+	packet(0x088D,5,clif->pHomMenu,2,4);
+	packet(0x0940,36,clif->pStoragePassword,0);
+#endif
+
 // 2015-12-16aRagexe
 #if PACKETVER >= 20151216
 	packet(0x089D,7,clif->pActionRequest,2,6);
@@ -3342,6 +3375,10 @@ packet(0x96e,-1,clif->ackmergeitems);
 
 #if PACKETVER >= 20151029
 	packetKeys(0x45B945B9,0x45B945B9,0x45B945B9); /* Dastgir */
+#endif
+
+#if PACKETVER >= 20151104
+	packetKeys(0x4C17382A,0x7ED174C9,0x29961E4F);
 #endif
 
 #if PACKETVER >= 20151216
