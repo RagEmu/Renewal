57bf42456602e6922f462fd2c72baf802c906c33
Haru <haru@dotalux.com>
2016-07-14 03:41:02 +0200

    Changed map_session_data::change_level_2nd and map_session_data::change_level_3rd to int
    Fixes several -Wsign-compare issues
    
    Signed-off-by: Haru <haru@dotalux.com>

--- src/map/pc.h
+++ src/map/pc.h
@@ -458,8 +458,8 @@ END_ZEROED_BLOCK;
 	int eventtimer[MAX_EVENTTIMER];
 	unsigned short eventcount; // [celest]
 
-	unsigned char change_level_2nd; // job level when changing from 1st to 2nd class [jobchange_level in global_reg_value]
-	unsigned char change_level_3rd; // job level when changing from 2nd to 3rd class [jobchange_level_3rd in global_reg_value]
+	int change_level_2nd; // job level when changing from 1st to 2nd class [jobchange_level in global_reg_value]
+	int change_level_3rd; // job level when changing from 2nd to 3rd class [jobchange_level_3rd in global_reg_value]
 
 	char fakename[NAME_LENGTH]; // fake names [Valaris]
 
@@ -569,7 +569,7 @@ END_ZEROED_BLOCK;
 
 	/* */
 	struct {
-		unsigned int second,third;
+		int second, third;
 	} sktree;
 
 	/**
