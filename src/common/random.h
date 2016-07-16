HeiSir2014/Hercules

Interface random.c

src/common/random.h
@@ -22,16 +22,25 @@
 #define COMMON_RANDOM_H
 
 #include "common/cbasetypes.h"
+#include "common/hercules.h"
+
+#define rnd() rnd->rnd()
+
+struct rnd_interface {
+	void (*init) (void);
+	void (*seed) (uint32);
+
+	int32 (*rnd) (void);// [0, SINT32_MAX]
+	uint32 (*roll) (uint32 dice_faces);// [0, dice_faces)
+	int32 (*value) (int32 min, int32 max);// [min, max]
+	double (*uniform) (void);// [0.0, 1.0)
+	double (*uniform53) (void);// [0.0, 1.0)
+};
 
 #ifdef HERCULES_CORE
-void rnd_init(void);
-void rnd_seed(uint32);
-
-int32 rnd(void);// [0, SINT32_MAX]
-uint32 rnd_roll(uint32 dice_faces);// [0, dice_faces)
-int32 rnd_value(int32 min, int32 max);// [min, max]
-double rnd_uniform(void);// [0.0, 1.0)
-double rnd_uniform53(void);// [0.0, 1.0)
+void rnd_defaults(void);
 #endif // HERCULES_CORE
+
+HPShared struct rnd_interface *rnd;
 
 #endif /* COMMON_RANDOM_H */

RagEmu/Renewal

src/common/random.h
@@ -22,17 +22,27 @@
 #ifndef COMMON_RANDOM_H
 #define COMMON_RANDOM_H
 
-#include "common/cbasetypes.h"
+#include "common/ragemu.h"
+
+struct rnd_interface {
+	void (*init) (void);
+	void (*final) (void);
+
+	void (*seed) (uint32);
+
+	int32 (*random) (void);// [0, SINT32_MAX]
+	uint32 (*roll) (uint32 dice_faces);// [0, dice_faces)
+	int32 (*value) (int32 min, int32 max);// [min, max]
+	double (*uniform) (void);// [0.0, 1.0)
+	double (*uniform53) (void);// [0.0, 1.0)
+};
+
+#define rnd() rnd->random()
 
 #ifdef RAGEMU_CORE
-void rnd_init(void);
-void rnd_seed(uint32);
-
-int32 rnd(void);// [0, SINT32_MAX]
-uint32 rnd_roll(uint32 dice_faces);// [0, dice_faces)
-int32 rnd_value(int32 min, int32 max);// [min, max]
-double rnd_uniform(void);// [0.0, 1.0)
-double rnd_uniform53(void);// [0.0, 1.0)
+void rnd_defaults(void);
 #endif // RAGEMU_CORE
+
+HPShared struct rnd_interface *rnd;
 
 #endif /* COMMON_RANDOM_H */