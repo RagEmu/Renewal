4578723d100fa243b3f6042195b50cbaf779711d
Haru <haru@dotalux.com>
2016-08-20 18:44:02 +0200

    Re-added HPM support for configuration settings
    Plugin settings should be relative to the the libconfig file root. For
    example, a configuration setting of type HPCT_CHAR will be relative to
    the root of conf/char/char-server.conf. In order to add a configuration
    entry inside the char_configuration block, the full configuration path
    (slash-delimited) should be passed to addCharConf(), as in the
    following example:
    
    `addCharConf("char_configuration/my_setting", my_parser_function);`
    
    Signed-off-by: Haru <haru@dotalux.com>

--- src/char/char.c
+++ src/char/char.c
@@ -5436,7 +5436,8 @@ bool char_sql_config_read(const char *filename, bool imported)
 		}
 	}
 
-	// TODO HPM->parseConf(w1, w2, HPCT_CHAR_INTER);
+	if (!HPM->parse_conf(&config, filename, HPCT_CHAR_INTER, imported))
+		retval = false;
 
 	libconfig->destroy(&config);
 	return retval;
@@ -5590,7 +5591,8 @@ bool char_config_read(const char *filename, bool imported)
 	if (!pincode->config_read(filename, &config, imported))
 		retval = false;
 
-	// TODO HPM->parseConf(w1, w2, HPCT_CHAR);
+	if (!HPM->parse_conf(&config, filename, HPCT_CHAR, imported))
+		retval = false;
 
 	ShowInfo("Done reading %s.\n", filename);
 
