c25474addd89201f0a1f5bdd9671642326888c4c
Andrei Karas <akaras@inbox.ru>
2016-09-08 19:43:56 +0300

    Add packet id clif_vendingreport (0x9e5 / ZC_DELETEITEM_FROM_MCSTORE2)
    Based on rAthena commit:
    commit 01adc9c3ff3c1c6f0f69f6f9f2db77eba20071d4
    Author: Napster <arokaice@live.com>
    Date:   Mon Dec 21 20:05:29 2015 +0700

--- src/map/vending.c
+++ src/map/vending.c
@@ -197,7 +197,7 @@ void vending_purchasereq(struct map_session_data* sd, int aid, unsigned int uid,
 		// Vending item
 		pc->additem(sd, &vsd->status.cart[idx], amount, LOG_TYPE_VENDING);
 		vsd->vending[vend_list[i]].amount -= amount;
-		clif->vendingreport(vsd, idx, amount);
+		clif->vendingreport(vsd, idx, amount, sd->status.char_id, (int)z);
 		pc->cart_delitem(vsd, idx, amount, 0, LOG_TYPE_VENDING);
 
 		// Print buyer's name
