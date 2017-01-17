7a308bb4272c45e56dc87d6dce3f989b74e06458
Michieru <michieru-kun@hotmail.com>
2017-01-17 02:23:48 +0100

    Update mob skill level to official

--- src/map/battle.c
+++ src/map/battle.c
@@ -1470,13 +1470,16 @@ int battle_calc_skillratio(int attack_type, struct block_list *src, struct block
 					skillratio += 25;
 					break;
 				case WZ_VERMILION:
-					if(sd) {
-						int per = 0;
-						while ((++per) < skill_lv)
-							skillratio += per * 5; //100% 105% 115% 130% 150% 175% 205% 240% 280% 325%
-					} else {
-						skillratio += 20 * skill_lv - 20; //Monsters use old formula
+				{
+					int interval = 0, per = interval, ratio = per;
+					while( (per++) < skill_lv ){
+						ratio += interval;
+						if(per%3==0) interval += 20;
 					}
+					if( skill_lv > 9 )
+						ratio -= 10;
+					skillratio += ratio;
+				}
 					break;
 				case NJ_HUUJIN:
 					skillratio += 50;
